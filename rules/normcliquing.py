7cdeee5346eb64e013b459b28ceef239bac22991
Priyesh Ranjan <pr8pf@umsystem.edu>
2024-04-10 16:50:27 -0500

    Fixing some things

--- rules/normcliquing.py
+++ rules/normcliquing.py
@@ -80,7 +80,7 @@ class Net(nn.Module):
         while len(Honest) == 0 :
             for i in range(n_clients) :
                 for j in range(i+1,n_clients) :
-                    if cs[i,j] < self.gamma*self.reputation[i] : 
+                    if 1 - cs[i,j] < self.gamma*self.reputation[i] : 
                         neighbors[i,j] = 1
                         neighbors[j,i] = 1
             Cliques = list(bronk([], [*range(n_clients)], [], neighbors))  
